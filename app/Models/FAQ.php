2b5ff9493df193955d07fb7b1fec72028bbaaccf
Mohammadbalata <balatam437@gmail.com>
2025-01-31 18:22:25 +0200

    finishing the crud system for the clinic's FAQs

--- app/Models/FAQ.php
+++ app/Models/FAQ.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class FAQ extends Model
 {
-    //
+
+    protected $fillable = [
+        'clinic_id', 'question', 'answer'
+    ];
+
+    public function clinic()
+    {
+        return $this->belongsTo(Clinic::class);
+    }
+
+
+    public static function rules()
+    {
+        return  [
+            'question'       => 'required|string|max:255',
+            'answer'    => 'required|string|max:500',
+            ];
+    }
+
 }
