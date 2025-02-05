0ff9847b3fc52d6ac189a576cb90655c90c19b70
khaleda-02 <khaleda.02f@gmail.com>
2025-02-05 13:15:40 +0200

    refactoring api contorller functions, enabled patient to add the cancellation reason

--- app/Http/Controllers/Api/AppointmentsController.php
+++ app/Http/Controllers/Api/AppointmentsController.php
@@ -22,18 +22,33 @@ class AppointmentsController extends Controller
             $request->validate(Appointment::rules($request));
             $procedure = Procedure::findOrFail($request->procedure_id); // to get the related doctor & room
             $room = Room::findOrFail($procedure->room_id); // to get related clinic
-            
+
+            // check if the slot available.
+
+            // testing: check if the procedure is provided in this clinic by comparing the clinic_id and room->clinic
             // TODO: make sure that to exclude just the (pending, confirm) appointment, while we need to consider the cancelled appointments as available slots
-            // check if the slot available. 
+            // handle the case of lunch_start & lunch_end. 
+
             // get the user, 
-            $patientExists = Patient::where('phone_number', $request->patient['phone_number'])->first();
-            // if the user doesn't exists, create it, 
-            if (!$patientExists) {
-                $newPatient = Patient::create($request->patient);
-                $request->merge(['patient_id' => $newPatient->id]);
+            if ($request->patient_id) {
+                //validate patient_id
+                $patient = Patient::find('id', $request->patient_id);
+                if (! $patient)
+                    return response()->json(['error' => 'patient not found.'], Response::HTTP_NOT_FOUND);
+                $request->merge(['patient_id' => $patient->id]);
             } else {
-                $request->merge(['patient_id' => $patientExists->id]);
+                $patientExists = Patient::where('phone_number', $request->patient['phone_number'])->first();
+                // if the user doesn't exists, create it, 
+                if (!$patientExists) {
+                    $newPatient = Patient::create($request->patient);
+                    $request->merge(['patient_id' => $newPatient->id]);
+                } else {
+                    // todo: ask mo about what should we do in this case. [1. make the patient to auth and return a patient  exists res, 2. use the founded patient]
+                    // $request->merge(['patient_id' => $patientExists->id]);
+                    return response()->json(['error' => 'patient already exists.'], Response::HTTP_BAD_REQUEST);
+                }
             }
+
 
             // create an appointment record, with pending status,
             $startTime = Carbon::createFromFormat('H:i', $request->start_time);
@@ -99,6 +114,7 @@ class AppointmentsController extends Controller
         try {
             $request->validate([
                 'phone_number' => 'required|string|regex:/^\+?[0-9]{7,15}$/',
+                'cancellation_reason' => 'nullable|string|min:7|max:250'
             ]);
 
             // get the patient by phone number
@@ -115,11 +131,12 @@ class AppointmentsController extends Controller
 
             // make sure that the user who booked this appointment.
             if ($appointment->patient->phone_number !== $request->phone_number) {
-                return response()->json(['error' => 'you are not allowed to confirm this appointment.'], Response::HTTP_FORBIDDEN);
+                return response()->json(['error' => 'you are not allowed to cancel this appointment.'], Response::HTTP_FORBIDDEN);
             }
 
             // update the appointment status to confirmed & returning res
             $appointment->status = AppointmentStatusEnum::Cancelled;
+            $request->cancellation_reason && $appointment->cancellation_reason = $request->cancellation_reason;
             $appointment->save();
 
             return response()->json([
