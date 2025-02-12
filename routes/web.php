9fee550e2cee45035fa3c2ef90b3db4accc11df9
khaleda-02 <khaleda.02f@gmail.com>
2025-02-12 16:25:11 +0200

    refactoing api routes, moving dashboard prfix&middleare into web.php file

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-
-// Route::get('/dashboard', function () {
-//     return view('dashboard.index');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-
-require __DIR__.'/dashboard.php';
-require __DIR__.'/auth.php';
+// instead of put the prefix & middleware in the app.php, I did it here, in case we need to add other web pages that doesn't require those prefix. 
+Route::prefix('dashboard')
+    ->middleware(['auth'])
+    ->as('dashboard.')
+    ->group(__DIR__ . '/dashboard.php');
+require __DIR__ . '/auth.php';
